207ac3ca895616c9ec0775d4cfd3665edb829d16
GRADY KNIGHT <gradyknight@gradys-mbp.lan>
2018-06-10 20:19:27 -0400

    shuffling

--- goat-rodeo/src/components/GameContainer.js
+++ goat-rodeo/src/components/GameContainer.js
@@ -81,8 +81,31 @@ class GameContainer extends Component {
     ],
   };
 
+  shuffle = (array) => {
+    // copied from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+
   handleCardClick = event => {
     console.log(event.target.id);
+    let newArray = this.shuffle(this.state.currentArray);
+    this.setState({currentArray:newArray});
   }
   render(){
     return (
